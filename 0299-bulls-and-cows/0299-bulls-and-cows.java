28804845b2e0ec3957f11765605c0c8fad897a07
khushitiwari <khushi162196@gmail.com>
2026-07-09 11:40:29 +0530

    Time: 3 ms (95.55%), Space: 43.4 MB (69.89%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution{
+    public String getHint(String secret, String guess){
+        int bulls=0;
+        int cows=0;
+        int[] s=new int[10];
+        int[] g=new int[10];
+        for(int i=0;i<secret.length();i++){
+            char a=secret.charAt(i);
+            char b=guess.charAt(i);
+            if(a==b){
+                bulls++;
+            }else{
+                s[a-'0']++;
+                g[b-'0']++;
+            }
+        }
+        for(int i=0;i<10;i++){
+            cows+=Math.min(s[i],g[i]);
+        }
+        return bulls+"A"+cows+"B";
+    }
+}
